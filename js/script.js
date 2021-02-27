64939d333954eca01dc4ff3026c82b67b0de93a0
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-27 22:54:29 +0100

    Add javascript code to get 'href' attribute from clicked link

--- js/script.js
+++ js/script.js
@@ -1,41 +1,60 @@
 'use strict';
 
+
+
+
 function titleClickHandler(event){
+
+  event.preventDefault();   //Disable default browser option for clicked links - go to ID
+  
   const clickedElement = this;
+
   console.log('Link was clicked!');
 
 
 
 
-/* [DONE] remove class 'active' from all article links  */
+/* Remove class 'active' from all article links  */
+  
   const activeLinks = document.querySelectorAll('.titles a.active'); //select all active links
     
-  for(let activeLink of activeLinks){                                //remove class active from all article links   
-    activeLink.classList.remove('active');
-  }
+  for(let activeLink of activeLinks){     //remove class active from all article links 
+  activeLink.classList.remove('active');
+
+}
 
 
 
-/* [IN PROGRESS] add class 'active' to the clicked link */
-  console.log('clickedElement:', clickedElement);
+
+/* Add class 'active' to the clicked link */
+
   clickedElement.classList.add('active');
 
+  console.log('clickedElement:', clickedElement);
+  
 
 
 
-/* [DONE] remove class 'active' from all articles */
+/* Remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
+
   }
 
 
 
-    /* get 'href' attribute from the clicked link */
-    /* find the correct article using the selector (value of 'href' attribute) */
-    /* add class 'active' to the correct article */
-}
+
+/* get 'href' attribute from the clicked link */
+
+  const articleVisible = clickedElement.getAttribute('href');
+
+
+
+
+
 
 const links = document.querySelectorAll('.titles a');
 
