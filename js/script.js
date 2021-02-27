82e86bb1422f2191250b97bb1d1984d910bb6d2b
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-27 16:31:38 +0100

    Add javascript code to remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -13,17 +13,16 @@ function titleClickHandler(event){
       activeLink.classList.remove('active');
     }
 
-
-
   /* add class 'active' to the clicked link */
-  
-
   /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
   /* get 'href' attribute from the clicked link */
-
   /* find the correct article using the selector (value of 'href' attribute) */
-
   /* add class 'active' to the correct article */
 }
 
