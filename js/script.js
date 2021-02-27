4f9e2cc045bdfba4d757ddffd4813d42ba307cb4
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-27 17:10:36 +0100

    Add javascript code to add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log('Link was clicked!');
-    console.log(event);
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
 
 
-  /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active'); //select all active links
+
+/* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active'); //select all active links
     
-    for(let activeLink of activeLinks){                                //remove class active from all article links   
-      activeLink.classList.remove('active');
-    }
+  for(let activeLink of activeLinks){                                //remove class active from all article links   
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
-  /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
 
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    }
 
-  /* get 'href' attribute from the clicked link */
-  /* find the correct article using the selector (value of 'href' attribute) */
-  /* add class 'active' to the correct article */
+/* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+
+
+
+/* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+
+
+    /* get 'href' attribute from the clicked link */
+    /* find the correct article using the selector (value of 'href' attribute) */
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
